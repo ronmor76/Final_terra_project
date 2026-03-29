b2c0808480b53692a03a3945316a80453aaf8864
ronmor76 <ronen.mordechai@gmail.com>
2026-03-29 19:04:03 +0300

    Update project files and fix gitignore

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "aws_iam_user_test" {
   source = "./modules/iam_user"
-  user_name = "Elinor1"
+  user_name = "Elinor2"
 }
 
 module "ec2" {
@@ -15,12 +15,12 @@ module "ec2" {
 
 module "iam_user" {
   source = "./modules/iam_user"
-  user_name = "Moti1"
+  user_name = "Moti2"
 }
 
 
 module "aws_s3_bucket" {
     source = "./modules/s3"
-    bucket_name = "s3newbucketfibibanktea1"
+    bucket_name = "s3newbucketfibibanktea2"
   
 }
